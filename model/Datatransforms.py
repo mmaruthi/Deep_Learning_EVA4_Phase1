da76dc0122e02f49065eaa896367db30b40f2ee2
Maruthi Srinivas Mudaragadda <48976860+mmaruthi@users.noreply.github.com>
2020-03-20 16:53:32 +0530

    Update Datatransforms.py

--- model/Datatransforms.py
+++ model/Datatransforms.py
@@ -1,4 +1,47 @@
 from torchvision import transforms
+import albumentations as A
+import albumentations.pytorch as AP
+import random
+import numpy as np
+
+
+# # class for Alubmentation Transforms # # 
+class AlbumentationTransforms:
+      def __init__(self,normalize=False, mean=None, stdev=None):
+            
+          self.normalize = normalize
+          self.mean      = mean 
+          self.stdev     = stdev 
+      
+      
+      def test_transforms(self):
+          transforms_list = [AP.ToTensor()]
+          if (self.normalize):
+             transforms_list.append(A.Normalize(self.mean,self.stdev))
+          return A.Compose(transforms_list)
+          
+      # Define a method for train data . It can have multiple transformations other than changing to tensor and normalizing
+      # so create your lists accordingly . One for before converting to tensor and normalization 
+      # one for after converting it to tensor and normalization
+      
+      
+      def train_transforms(self , before_norm=None, after_norm=None):
+          if before_norm:
+             transforms_list = before_norm 
+             transforms_list.append(AP.ToTensor())
+          else:
+             transforms_list = [AP.ToTensor()]
+             
+          if (self.normalize):
+             transforms_list.append(A.Normalize(self.mean,self.stdev))
+           
+          if after_norm:
+             transforms_list.extend(after_norm)
+           
+          return A.Compose(transforms_list)
+
+      
+      
 
 # # class for Transformations ## 
 class Transforms_custom:
